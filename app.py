8ea7b7e93ddee8e05a4f9e04ca13b40f941e198e
Matthew Herron <matthew.herron77@gmail.com>
2017-03-05 17:54:44 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,36 @@
+class Directory:
+	def __init__(self, max_users):
+		self.user_count = 0
+		self.max_users = max_users
+		self.directory = list(None for x in range(0, self.max_users))
+
+	def promptNewUser(self):
+		single_user = list(None for x in range(0, 4))
+		first_name = raw_input("Enter first name > ")
+		last_name = raw_input("Enter last name > ")
+		phone = raw_input("Enter phone number > ")
+		email = raw_input("Enter email address > ")
+		single_user[0] = first_name
+		single_user[1] = last_name
+		single_user[2] = phone
+		single_user[3] = email
+		
+		return single_user
+
+	def addUser(self, user):
+		if self.user_count == self.max_users:
+			print "Sorry directory is full..."
+
+		else:
+			print "Inserting user into index " + str(self.user_count)
+			self.directory[self.user_count] = user
+			self.user_count += 1
+
+
+product_team = Directory(2)
+
+for i in range(0, product_team.max_users):
+	new_member = product_team.promptNewUser()
+	product_team.addUser(new_member)
+
+print product_team.directory
