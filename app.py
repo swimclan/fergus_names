bac293c847e935f54ebed77ef20c161d9b6f3fa1
Matthew Herron <matthew.herron77@gmail.com>
2017-03-05 18:18:38 -0600

    Added id numbers to each user

--- app.py
+++ app.py
@@ -5,17 +5,18 @@ class Directory:
 		self.directory = list(None for x in range(0, self.max_users))
 
 	def promptNewUser(self):
-		single_user = list(None for x in range(0, 5))
+		single_user = list(None for x in range(0, 6))
 		first_name = raw_input("Enter first name > ")
 		last_name = raw_input("Enter last name > ")
 		phone = raw_input("Enter phone number > ")
 		email = raw_input("Enter email address > ")
 		city = raw_input("Enter city > ")
-		single_user[0] = first_name
-		single_user[1] = last_name
-		single_user[2] = phone
-		single_user[3] = email
-		single_user[4] = city
+		single_user[0] = self.user_count
+		single_user[1] = first_name
+		single_user[2] = last_name
+		single_user[3] = phone
+		single_user[4] = email
+		single_user[5] = city
 		
 		return single_user
 
