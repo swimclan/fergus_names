09e855e99926808d1e47ccefb7b8d907571c9696
Matthew Herron <matthew.herron77@gmail.com>
2017-03-05 19:11:56 -0600

    added comments to the code for clarity

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 class Directory:
+	# Initialize the properties of the directory inluding a list of None type objects
+	# as if it is a JAVA array class being initialized.
 	def __init__(self, max_users):
 		self.user_count = 0
 		self.max_users = max_users
 		self.directory = list(None for x in range(0, self.max_users))
 
+	# Interactively prompt for attributes for a single user and then 
+	# return the newly created user
 	def promptNewUser(self):
 		single_user = list(None for x in range(0, 6))
 		first_name = raw_input("Enter first name > ")
@@ -20,6 +24,9 @@ class Directory:
 		
 		return single_user
 
+	# Add a single user to the directory of the class object.  Test
+	# for error condition when adding a user after the maximum 
+	# number of users has already been realized
 	def addUser(self, user):
 		if self.user_count == self.max_users:
 			print "Sorry directory is full..."
@@ -28,9 +35,11 @@ class Directory:
 			self.directory[self.user_count] = user
 			self.user_count += 1
 
+	# Fetch the current user count for all users
 	def getUserCount(self):
 		print self.user_count
 
+	# Remove a user with a specific id from the directory
 	def deleteUser(self, id):
 		del self.directory[id]
 		return self.directory
